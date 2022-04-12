5dfbd891a343c776030c7091f61bf8294db94b51
FedericoOlguin <fede_olguin_10@outlook.com.ar>
2022-04-12 17:10:32 -0300

    Sign in /up

--- src/navigation/Drawer.js
+++ src/navigation/Drawer.js
@@ -1,6 +1,8 @@
 import * as React from 'react';
 import { createDrawerNavigator } from '@react-navigation/drawer';
 import OpenPag from '../screens/open';
+import FormSignIn from "../screens/signInScreen"
+import FormSignUp from "../screens/signUpScreen"
 
 const Drawer = createDrawerNavigator();
 
@@ -8,6 +10,8 @@ function DrawerNavigator() {
   return (
     <Drawer.Navigator initialRouteName="Open">
       <Drawer.Screen name="Open" component={OpenPag} />
+      <Drawer.Screen name="SignIn" component={FormSignIn} />
+      <Drawer.Screen name="SignUp" component={FormSignUp} />
     </Drawer.Navigator>
   )
 }
---
+++ src/screens/signUpScreen.js
@@ -0,0 +1,163 @@
+import { connect } from 'react-redux';
+// import usuariosActions from "../../redux/actions/usuariosActions"
+import React, { useState } from 'react';
+import { Button, TextInput, View, ImageBackground, ScrollView, Text, TouchableOpacity, Feather } from 'react-native';
+import { FontAwesome } from '@expo/vector-icons';
+
+const FormSignUp = (props) => {
+    const [signUp, setSignUp] = useState({
+        firstName: "",
+        lastName: "",
+        email: "",
+        password: "",
+        imageUrl: "",
+        country: "",
+    })
+
+    function enviar() {
+
+        alert("Under construction")
+
+        props.signUp(signUp)
+
+    }
+
+
+    return (
+        <ScrollView>
+            <ImageBackground source={{ uri: "https://papers.co/wallpaper/papers.co-vh91-watercolor-texture-dark-black-pattern-36-3840x2400-4k-wallpaper.jpg" }} style={{ width: "100 %", display: 'flex', alignItems: 'center', justifyContent: 'center', height: 700 }}>
+                <View style={{ backgroundColor: '#000', width: '90%', minHeight: 500, alignSelf: 'center', backgroundColor: "#f3a446", borderRadius: 10 }}>
+                    <Text style={{ fontSize: 28, fontWeight: 'bold', textAlign: 'center', fontFamily: 'serif', paddingTop: 20, color: '#fff', textShadowColor: '#17F5F9', marginBottom: 30 }}>
+                        Sign Up
+                    </Text>
+                    <TextInput
+                        style={{ height: 40, borderColor: 'white', backgroundColor: "rgba(39, 37, 37, 0.833)", borderWidth: 1, padding: 10, width: '80%', alignSelf: 'center', marginTop: 10, }}
+                        underlineColorAndroid="transparent"
+                        placeholder="First Name"
+                        placeholderTextColor="white"
+                        textAlign='justify'
+                        borderRadius={5}
+                        padding={10}
+                        fontSize={15}
+                        color="#fff"
+                        marginBottom={10}
+
+                        onChangeText={(text) => { setSignUp({ ...signUp, firstName: text }) }}
+
+                    />
+
+                    <TextInput
+                        style={{ height: 40, borderColor: "white", backgroundColor: "rgba(39, 37, 37, 0.833)", borderWidth: 1, padding: 10, width: '80%', alignSelf: 'center', marginTop: 10, }}
+                        underlineColorAndroid="transparent"
+                        placeholder="Last  Name"
+                        placeholderTextColor="white"
+                        textAlign='justify'
+                        borderRadius={5}
+                        padding={10}
+                        fontSize={15}
+                        color="#fff"
+                        marginBottom={10}
+                        onChangeText={(text) => { setSignUp({ ...signUp, lastName: text }) }}
+
+                    />
+
+                    <TextInput
+                        style={{ height: 40, borderColor: "white", backgroundColor: "rgba(39, 37, 37, 0.833)", borderWidth: 1, padding: 10, width: '80%', alignSelf: 'center', marginTop: 10, }}
+                        underlineColorAndroid="transparent"
+                        placeholder="E-mail"
+                        placeholderTextColor="white"
+                        textAlign='justify'
+                        borderRadius={5}
+                        padding={10}
+                        fontSize={15}
+                        color="#fff"
+                        marginBottom={10}
+
+                        onChangeText={(text) => { setSignUp({ ...signUp, email: text }) }}
+
+                    />
+
+                    <TextInput
+                        style={{ height: 40, borderColor: "white", backgroundColor: "rgba(39, 37, 37, 0.833)", borderWidth: 1, padding: 10, width: '80%', alignSelf: 'center', marginTop: 10, }}
+                        underlineColorAndroid="transparent"
+                        placeholder="Password"
+                        placeholderTextColor="white"
+                        textAlign='justify'
+                        borderRadius={5}
+                        padding={10}
+                        fontSize={15}
+                        color="#fff"
+                        marginBottom={10}
+                       
+
+                        onChangeText={(text) => { setSignUp({ ...signUp, password: text }) }}
+
+                    />
+
+                    <TextInput
+                        style={{ height: 40, borderColor: "white", backgroundColor: "rgba(39, 37, 37, 0.833)", borderWidth: 1, padding: 10, width: '80%', alignSelf: 'center', marginTop: 10, }}
+                        underlineColorAndroid="transparent"
+                        placeholder="Photo URL"
+                        placeholderTextColor="white"
+                        textAlign='justify'
+                        borderRadius={5}
+                        padding={10}
+                        fontSize={15}
+                        color="#fff"
+                        marginBottom={10}
+
+
+                        onChangeText={(text) => { setSignUp({ ...signUp, imageUrl: text }) }}
+
+                    />
+                    <TextInput
+                        style={{ height: 40, borderColor: "white", backgroundColor: "rgba(39, 37, 37, 0.833)", borderWidth: 1, padding: 10, width: '80%', alignSelf: 'center', marginTop: 10, }}
+                        underlineColorAndroid="transparent"
+                        placeholder="Country"
+                        placeholderTextColor="white"
+                        textAlign='justify'
+                        borderRadius={5}
+                        padding={10}
+                        fontSize={15}
+                        color="#fff"
+                        marginBottom={10}
+
+
+                        onChangeText={(text) => { setSignUp({ ...signUp, country: text }) }}
+
+                    />
+                    <TouchableOpacity
+                        onPress={enviar}
+                        title="Home"
+                        fontFamily='serif'
+                        style={{
+                            alignItems: "center", justifyContent: "center", marginTop: 20, marginBottom: 20, backgroundColor: "black",
+                            width: 70, height: 40, borderRadius: 5, marginLeft: "auto", marginRight: "auto"
+                        }}
+
+
+                    ><Text style={{ color: "brown", fontSize: 20 }} >
+                            <FontAwesome name="send-o" size={24} color="white" />
+                        </Text>
+                    </TouchableOpacity>
+
+                </View>
+
+            </ImageBackground>
+        </ScrollView>
+    );
+}
+
+
+// const mapDispatchToProps = {
+//     signUp: usuariosActions.signUp
+// }
+
+// const mapStateToProps = (state) => {
+//     return {
+//         user: state.usuariosReducer.user
+
+//     }
+// }
+
+export default connect(null, null)(FormSignUp)
