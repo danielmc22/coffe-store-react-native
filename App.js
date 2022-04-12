699419b8c8448f53f747304632ca9d17ce872a78
Sebazt <juan.londono29@udea.edu.co>
2022-04-12 08:38:18 -0500

    iniciacializando movible

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={{}}> ----------------------m----------------------</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +13,9 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#1d1d1d',
     alignItems: 'center',
     justifyContent: 'center',
+    color:'#fff',
   },
 });
