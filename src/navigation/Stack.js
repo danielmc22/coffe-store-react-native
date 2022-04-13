ad738523b2beca57ae1d903a0565e00838778098
FedericoOlguin <fede_olguin_10@outlook.com.ar>
2022-04-13 17:46:40 -0300

    Shop conectado en el stack

--- src/navigation/Stack.js
+++ src/navigation/Stack.js
@@ -1,15 +1,21 @@
 import * as React from 'react';
 import { createStackNavigator } from '@react-navigation/stack';
+import Shop from '../screens/Shop';
 
 const Stack = createStackNavigator();
 
 function StoreNavigator() {
   return (
-    <Stack.Navigator initialRouteName=''
+    <Stack.Navigator initialRouteName='Store'
       screenOptions={{ headerBackTitle: "Back" }}
     >
-      <Stack.Screen name='' component={""}
+      <Stack.Screen name='Store' component={Shop}
+        options={{ headerShown: false, }} />
+      <Stack.Screen name='Detalle' component={Shop}
         options={{ headerShown: false, }} />
     </Stack.Navigator >
   )
 }
+
+
+export default StoreNavigator
